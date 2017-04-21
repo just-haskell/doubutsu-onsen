c1285d277d0fcbbdfda77237cc94a8aa49b44d77
Kei Hibino <ex8k.hibino@gmail.com>
2017-04-22 00:43:31 +0900

    fix sequence type.

--- backend/src/Database/DoubutsuOnsen/Entity/Onsen.hs
+++ backend/src/Database/DoubutsuOnsen/Entity/Onsen.hs
@@ -5,7 +5,7 @@ module Database.DoubutsuOnsen.Entity.Onsen where
 
 import GHC.Generics (Generic)
 import Prelude hiding (seq)
-import Data.Int (Int64)
+import Data.Int (Int32)
 
 import Database.Relational.Extra.Sequence
   (SequenceDerivable (..), unsafeSpecifySequence, SequenceFromTable (..))
@@ -17,7 +17,7 @@ $(defineTable [] "DOUBUTSU" "onsen"
 $(defineTable [] "DOUBUTSU" "onsen_seq"
   [''Generic])
 
-instance SequenceDerivable OnsenSeq Int64 where
+instance SequenceDerivable OnsenSeq Int32 where
   deriveSequence = unsafeSpecifySequence seq seq'
 
-instance SequenceFromTable Onsen OnsenSeq Int64
+instance SequenceFromTable Onsen OnsenSeq Int32
