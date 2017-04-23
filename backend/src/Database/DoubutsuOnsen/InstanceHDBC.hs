5eb16ab5d1a16ee52cb6b54592d58389751a91d3
Kei Hibino <ex8k.hibino@gmail.com>
2017-04-23 17:04:56 +0900

    apply generalized ToSql instances.

--- backend/src/Database/DoubutsuOnsen/InstanceHDBC.hs
+++ backend/src/Database/DoubutsuOnsen/InstanceHDBC.hs
@@ -10,7 +10,19 @@ import Database.Record (FromSql)
 import Database.HDBC (SqlValue)
 import Database.HDBC.Record.Persistable ()
 
+import Data.DoubutsuOnsen.Onsen (Onsen)
+import qualified Data.DoubutsuOnsen.Onsen as Onsen
 import Data.DoubutsuOnsen.Doubutsu (Doubutsu)
+import qualified Data.DoubutsuOnsen.Doubutsu as Doubutsu
+import Data.DoubutsuOnsen.Item (Item)
+import qualified Data.DoubutsuOnsen.Item as Item
 
+instance FromSql SqlValue Onsen
+instance (FromSql SqlValue a, FromSql SqlValue b) => FromSql SqlValue (Onsen.Status a b)
 
 instance FromSql SqlValue a => FromSql SqlValue (Doubutsu a)
+instance FromSql SqlValue a => FromSql SqlValue (Doubutsu.SlotStatus a)
+instance FromSql SqlValue Doubutsu.Coord
+
+instance FromSql SqlValue Item
+instance FromSql SqlValue Item.SlotStatus
